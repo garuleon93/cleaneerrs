0e145ee5f3b65e6da01f55c0ba6458c77a703d2f
garuleon93 <patricio.leonb93@gmail.com>
2026-05-06 13:15:39 -0500

    Update app.py

--- aplicacion/app.py
+++ aplicacion/app.py
@@ -1,8 +1,20 @@
 from controlador.controlador_principal import Controlador_Principal
+from pathlib import Path
 import flet as ft
+import sys
+#   Coloco esto para arreglar ejecución
+repo_root = Path(__file__).resolve().parents[1]  # .../EmpresaElectrica
+if str(repo_root) not in sys.path:
+    sys.path.insert(0, str(repo_root))
+
+
+# --- APPEND END ---
+repo_root = Path(__file__).resolve().parents[1]   # .../EmpresaElectrica
+if str(repo_root) not in sys.path:
+    sys.path.insert(0, str(repo_root))
 
 def main(page: ft.Page):
-    page.bgcolor = ft.colors.WHITE
+    page.bgcolor = ft.Colors.WHITE
     page.title="Gestor de Contratos"
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
     #age.theme = ft.Theme(color_scheme=ft.colorScheme(primary=ft.colors.BLACK))
@@ -10,7 +22,7 @@ def main(page: ft.Page):
 
     controlador = Controlador_Principal(page)
     controlador.mostrar_vista()
-
  
 
-ft.app(target=main) 
+if __name__ == "__main__":
+    ft.app(target=main)
